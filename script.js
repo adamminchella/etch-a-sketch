b5222d981452f0e148705e6013902f458a1feced
Adam Minchella <adamminchella@msn.com>
2022-04-13 14:54:36 +0100

    Add reload button

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 const gridContainer = document.querySelector(".grid-container");
 const grid = document.querySelector(".grid");
+const reloadButton = document.querySelector(".reload");
 const rows = 16;
 const columns = rows;
 const numberOfGridCells = rows * columns;
+
+reloadButton.addEventListener("click", reloadGrid);
+
+function reloadGrid() {
+  clearGrid();
+  createGrid();
+}
+
+function clearGrid() {
+  grid.innerHTML = "";
+}
 
 function createGrid() {
   grid.setAttribute("style", `grid-template-columns: repeat(${columns}, 1fr)`);
   for (let i = 1; i <= numberOfGridCells; i++) {
     const gridCell = document.createElement("div");
-    gridCell.style.border = "1px solid black";
     gridCell.classList.add("grid-cell");
     grid.appendChild(gridCell);
   }
+  configureGridCells();
 }
-
-createGrid();
 
 function addColor(gridCell) {
   gridCell.setAttribute("style", "background-color: black");
 }
 
-const gridCells = document.querySelectorAll(".grid-cell");
-gridCells.forEach((gridCell) => {
-  gridCell.addEventListener("mouseover", () => {
-    addColor(gridCell);
+function configureGridCells() {
+  const gridCells = document.querySelectorAll(".grid-cell");
+  gridCells.forEach((gridCell) => {
+    gridCell.addEventListener("mouseover", () => {
+      addColor(gridCell);
+    });
   });
-});
+}
+
+window.onload = createGrid();
