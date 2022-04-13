09d462e7fda8613b951f1c7377ca8845367ceb0a
Adam Minchella <adamminchella@msn.com>
2022-04-13 15:17:36 +0100

    Add slider that allows user to select grid size

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const gridContainer = document.querySelector(".grid-container");
 const grid = document.querySelector(".grid");
 const reloadButton = document.querySelector(".reload");
-const rows = 16;
-const columns = rows;
-const numberOfGridCells = rows * columns;
+const size = document.querySelector(".slider");
+const sizeInfo = document.querySelector(".slider-text");
+let rows = 16;
 
+size.addEventListener("input", changeGridSize);
 reloadButton.addEventListener("click", reloadGrid);
+
+function changeGridSize() {
+  rows = size.value;
+  sizeInfo.innerHTML = `Size: ${rows} X ${rows}`;
+}
 
 function reloadGrid() {
   clearGrid();
@@ -17,17 +23,13 @@ function clearGrid() {
 }
 
 function createGrid() {
-  grid.setAttribute("style", `grid-template-columns: repeat(${columns}, 1fr)`);
-  for (let i = 1; i <= numberOfGridCells; i++) {
+  grid.setAttribute("style", `grid-template-columns: repeat(${rows}, 1fr)`);
+  for (let i = 1; i <= rows * rows; i++) {
     const gridCell = document.createElement("div");
     gridCell.classList.add("grid-cell");
     grid.appendChild(gridCell);
   }
   configureGridCells();
-}
-
-function addColor(gridCell) {
-  gridCell.setAttribute("style", "background-color: black");
 }
 
 function configureGridCells() {
@@ -37,6 +39,10 @@ function configureGridCells() {
       addColor(gridCell);
     });
   });
+}
+
+function addColor(gridCell) {
+  gridCell.setAttribute("style", "background-color: black");
 }
 
 window.onload = createGrid();
